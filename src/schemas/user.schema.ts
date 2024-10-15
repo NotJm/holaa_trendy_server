a15c936a74a217a45aa6b3d19745443445029e3c
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-10-14 23:16:12 -0600

    Actualizacion package

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsBoolean, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
